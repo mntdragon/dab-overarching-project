4ed65c1ef4f051549e676f33a1b864b5d8f58e11
Bonny Ham (Boe) <mntdragon96@gmail.com>
2026-03-06 00:39:47 +0200

    Adding multiple comments. Comments are added to the end of the list. Comments persist between page reloads.

---
+++ 5-content-state/recipe-app/src/components/Comments.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { commentState } from '../states/commentState.svelte.js';
+
+  let newComment = $state('');
+
+  function handleAdd() {
+    if (newComment.trim()) {
+      commentState.addComment(newComment);
+      newComment = '';
+    }
+  }
+</script>
+
+<input type="text" bind:value={newComment} placeholder="Write a comment..." />
+<button onclick={handleAdd}>Add Comment</button>
+
+<h2>Comments</h2>
+
+<ul>
+  {#each commentState.comments as comment}
+    <li>{comment}</li>
+  {/each}
+</ul>
