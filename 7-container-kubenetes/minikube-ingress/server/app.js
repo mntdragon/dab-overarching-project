6bf53e19503d66a443d3033c44dd48c09707d89e
Bonny Saigonese <bonny@Bonnys-MacBook-Air.local>
2026-04-06 15:18:43 +0300

    adding a configmap file and a secret file

--- 7-container-kubenetes/minikube-ingress/server/app.js
+++ 7-container-kubenetes/minikube-ingress/server/app.js
@@ -1,9 +1,14 @@
 import { Hono } from "@hono/hono";
+import { cors } from "@hono/hono/cors";
+import { logger } from "@hono/hono/logger";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello world!" });
-});
+const message = Deno.env.get("WELCOME_MESSAGE") || "Hello world!";
+
+app.use("/*", cors());
+app.use("/*", logger());
+
+app.get("/", (c) => c.json({ message }));
 
 export default app;
